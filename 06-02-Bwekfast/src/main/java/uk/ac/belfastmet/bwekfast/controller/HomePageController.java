b8e045ed7fd4ec166341e407a20fbda156b2b9d1
EDG17149879 <EDG17149879@CLCLRCAC11.belfastmet.int>
2017-12-12 12:03:05 +0000

    Missed file from 12th dec commit

--- 06-02-Bwekfast/src/main/java/uk/ac/belfastmet/bwekfast/controller/HomePageController.java
+++ 06-02-Bwekfast/src/main/java/uk/ac/belfastmet/bwekfast/controller/HomePageController.java
@@ -1,8 +1,12 @@
 package uk.ac.belfastmet.bwekfast.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.belfastmet.bwekfast.repository.CerealRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,9 +14,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
 	
 	
+	@Autowired
+	CerealRepository cerealRepository;
+	
+	
+	public HomePageController(CerealRepository cerealRepository) {
+		super();
+		this.cerealRepository = cerealRepository;
+	}
+
+
 	@GetMapping("/")
 	public String home(Model model)
 	{
+		model.addAttribute("cereals", cerealRepository.findAll());
 		return "homepage";
 	}
 
